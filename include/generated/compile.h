44f4f278562d53aa8eb846d5a4a4ad2ab301a18a
Carlos Gallego <gallegomosquerajeancarlos@gmail.com>
2017-07-06 16:24:06 -0500

    SELinux to Permissive and Mali driver integration

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Jul 6 15:52:09 COT 2017"
+#define LINUX_COMPILE_BY "jean"
+#define LINUX_COMPILE_HOST "jean-i5"
+#define LINUX_COMPILER "gcc version 4.9.4 20160401 (optimized) (UBERTC-4.9.4) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.10.17-BlackLkernel-g6bae7c1-dirty"
